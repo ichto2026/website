9eb155f8b94890882eb6a116b4b6c6e5a0af3b5d
Jeonguk Choi <91456421+cjeonguk@users.noreply.github.com>
2025-08-31 00:21:15 +0900

    Set Favicon

--- app/root.tsx
+++ app/root.tsx
@@ -21,6 +21,18 @@ export const links: Route.LinksFunction = () => [
     rel: "stylesheet",
     href: "https://fonts.googleapis.com/css2?family=Inter:ital,opsz,wght@0,14..32,100..900;1,14..32,100..900&display=swap",
   },
+  {
+    rel: "icon",
+    type: "image/png",
+    sizes: "32x32",
+    href: "/cropped-Olimpiada-de-chimie-logo-orizontal-1-1-32x32.png",
+  },
+  {
+    rel: "icon",
+    type: "image/png",
+    sizes: "192x192",
+    href: "/cropped-Olimpiada-de-chimie-logo-orizontal-1-1-192x192.png",
+  },
 ];
 
 export function Layout({ children }: { children: React.ReactNode }) {
