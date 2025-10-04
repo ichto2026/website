1327fe83192013cec549ef750747c50ae221204e
Jeonguk Choi <91456421+cjeonguk@users.noreply.github.com>
2025-10-05 02:18:33 +0900

    feat: Add results page

---
+++ app/routes/results.tsx
@@ -0,0 +1,27 @@
+import HeroImage from "~/components/HeroImage";
+import type { Route } from "./+types/about";
+import { Link } from "react-router";
+
+export function meta({}: Route.MetaArgs) {
+  return [
+    { title: "Results - IChTo 2026" },
+    { name: "description", content: "Results of International Chemistry Tournament 2026" },
+  ];
+}
+
+export default function Results() {
+  return (
+    <div className="min-h-screen">
+      {/* Hero Section with About.jpg background */}
+      <HeroImage title="Results" imageUrl="/about.jpg" />
+
+      {/* Results Section */}
+      <div className="container mx-auto px-4 text-center">
+        <div className="text-gray-600">
+          <p className="my-4">Results information will be updated during the tournament.</p>
+          <p className="my-4"><Link to="/past" className="text-blue-800">View Past Results</Link></p>
+        </div>
+      </div>
+    </div>
+  );
+}
