6b97d46c5ac24846fd0d6f6905217ad78bb25189
Jeonguk Choi <91456421+cjeonguk@users.noreply.github.com>
2025-10-04 21:03:36 +0900

    fix: set mobile layout for rules page

--- app/routes/rules.tsx
+++ app/routes/rules.tsx
@@ -11,17 +11,17 @@ export function meta({}: Route.MetaArgs) {
 }
 
 function Btn({
-    text,
     selected = false,
     onClick,
+    children,
   }: {
-    text: string;
+    children?: React.ReactNode;
     selected?: boolean;
     onClick?: () => void;
   }) {
   return (
-    <button className={`px-5 py-2 rounded-2xl transition-colors font-medium text-lg ${selected ? "bg-blue-800 text-white" : "bg-gray-100 text-gray-800 hover:bg-blue-100"}`} onClick={onClick}>
-      {text}
+    <button className={`min-h-12 min-w-12 px-5 py-2 rounded-2xl transition-colors font-medium text-lg ${selected ? "bg-blue-800 text-white" : "bg-gray-100 text-gray-800 hover:bg-blue-100"}`} onClick={onClick}>
+      {children}
     </button>
   );
 }
@@ -42,16 +42,26 @@ export default function Rules() {
       <HeroImage title="Rules and Format" imageUrl="/about.jpg" />
 
       {/* Rules Section */}
-      <div className="container mx-auto px-4 text-center my-16 h-xl flex justify-around items-center">
-        <Btn text="Procedure" selected={selected === "Procedure"} onClick={() => {setSelected("Procedure"); setContent(contentProcedure);}} />
-        <div className="flex-1 h-px bg-gray-300 mx-4"></div>
-        <Btn text="Reporter" selected={selected === "Reporter"} onClick={() => {setSelected("Reporter"); setContent(contentReporter);}} />
-        <div className="flex-1 h-px bg-gray-300 mx-4"></div>
-        <Btn text="Opponent" selected={selected === "Opponent"} onClick={() => {setSelected("Opponent"); setContent(contentOpponent);}} />
-        <div className="flex-1 h-px bg-gray-300 mx-4"></div>
-        <Btn text="Reviewer" selected={selected === "Reviewer"} onClick={() => {setSelected("Reviewer"); setContent(contentReviewer);}} />
-        <div className="flex-1 h-px bg-gray-300 mx-4"></div>
-        <Btn text="Jury" selected={selected === "Jury"} onClick={() => {setSelected("Jury"); setContent(contentJury);}} />
+      <div className="container mx-auto px-4 text-center my-16 flex justify-around items-center">
+        <Btn selected={selected === "Procedure"} onClick={() => {setSelected("Procedure"); setContent(contentProcedure);}}>
+          <span className="hidden md:inline">Procedure</span>
+        </Btn>
+        <div className="flex-1 h-px bg-gray-300 md:mx-4"></div>
+        <Btn selected={selected === "Reporter"} onClick={() => {setSelected("Reporter"); setContent(contentReporter);}}>
+          <span className="hidden md:inline">Reporter</span>
+        </Btn>
+        <div className="flex-1 h-px bg-gray-300 md:mx-4"></div>
+        <Btn selected={selected === "Opponent"} onClick={() => {setSelected("Opponent"); setContent(contentOpponent);}}>
+          <span className="hidden md:inline">Opponent</span>
+        </Btn>
+        <div className="flex-1 h-px bg-gray-300 md:mx-4"></div>
+        <Btn selected={selected === "Reviewer"} onClick={() => {setSelected("Reviewer"); setContent(contentReviewer);}}>
+          <span className="hidden md:inline">Reviewer</span>
+        </Btn>
+        <div className="flex-1 h-px bg-gray-300 md:mx-4"></div>
+        <Btn selected={selected === "Jury"} onClick={() => {setSelected("Jury"); setContent(contentJury);}}>
+          <span className="hidden md:inline">Jury</span>
+        </Btn>
       </div>
       <ImageNText
         title={selected}
