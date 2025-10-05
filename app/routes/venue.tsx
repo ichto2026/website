8e645c4e39c4d76111d6f4ca30c4f2a24420656a
Jeonguk Choi <91456421+cjeonguk@users.noreply.github.com>
2025-10-06 02:21:10 +0900

    fix: mobile space change

--- app/routes/venue.tsx
+++ app/routes/venue.tsx
@@ -67,7 +67,7 @@ export default function Venue() {
             </div>
           </div>
         </div>
-        <div className="max-w-4xl mx-auto px-4 py-16 flex flex-col md:flex-row items-center justify-around space-x-4">
+        <div className="max-w-4xl mx-auto px-4 py-16 flex flex-col md:flex-row items-start justify-around space-x-4 space-y-4">
           <div className="">
             <h3 className="text-2xl font-bold text-gray-900 mb-4">The way from the airport</h3>
             <p className="text-gray-700 mb-8">
